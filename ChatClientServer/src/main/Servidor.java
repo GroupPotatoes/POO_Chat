8cd6b4ff3dae934ed087a560e4a5a145dab9e45b
Maiara Rodrigues <maiaraolirod@yahoo.com.br>
2015-11-06 18:21:53 -0200

    Adicionada Versão Enviada Pro André

---
+++ ChatClientServer/src/main/Servidor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import service.ServidorService;
+
+/**
+ *
+ * @author Maiara
+ */
+public class Servidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ServidorService();
+    }
+}
